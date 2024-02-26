9bc140dc35ee01787682821498d666ad71bb7bad
christian <8876980+cpinotossi@users.noreply.github.com>
2024-02-26 10:16:17 +0100

    overwrite-policy-nsg (#11)
    * update READNE.md
    
    * overwrite-policy-nsg
    
    ---------
    
    Co-authored-by: operator-cptblue <160131186+operator-cptblue@users.noreply.github.com>

--- locals.alz.tf
+++ locals.alz.tf
@@ -4,26 +4,3 @@ locals {
     v.location => v.id
   }
 }
-
-locals {
-  archetype_config_overrides = {
-    landing-zones = {
-      archetype_id = "es_landing_zones"
-      parameters = {
-        Deny-Subnet-Without-Nsg = {
-          effect = "Audit"
-        }
-      }
-      access_control = {}
-    }
-    corp = {
-      archetype_id = "es_corp"
-      parameters = {
-        Deny-Public-Endpoints = {
-          ACRPublicIpDenyEffect = "Audit"
-        }
-      }
-      access_control = {}
-    }
-  }
-}
